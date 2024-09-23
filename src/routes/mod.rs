8be3507f43d6864c1bb369cf645b220a68cf06f2
Samyakt <isamyakt17@gmail.com>
2024-09-23 17:14:30 +0530

    string  shows in http

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,12 @@
-use axum::{routing::get, Router};
+use axum::{routing::{get, post}, Router};
 
 mod hello_world;
+mod text_body_string;
 
 use hello_world::hello_world;
+use text_body_string::text_body_string;
 
 pub fn create_routes() -> Router {
     Router::new().route("/", get(hello_world))
+        .route("/text_body_string", post(text_body_string))  // shows http `body` in string text
 }
---
+++ src/routes/text_body_string.rs
@@ -0,0 +1,3 @@
+pub async fn text_body_string(body: String) -> String {
+    body
+}
