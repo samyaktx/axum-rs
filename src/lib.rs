1c142300b94e589a2a909f44194601efccf2bf3d
Samyakt <isamyakt17@gmail.com>
2024-09-23 16:56:08 +0530

    tokio tcp listener and axum service

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use routes::create_routes;
+
+mod routes;
+
+pub async fn run() {
+    let app = create_routes();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
