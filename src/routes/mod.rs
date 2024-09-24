f82a889b9c80bb6cac2221c78d9cea0395118ace
Samyakt <isamyakt17@gmail.com>
2024-09-24 21:26:43 +0530

    middleware message for shared data

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,4 @@
-use axum::{http::Method, routing::{get, post}, Router};
+use axum::{http::Method, routing::{get, post}, Extension, Router};
 use tower_http::cors::{Any, CorsLayer};
 
 mod hello_world;
@@ -8,7 +8,7 @@ mod path_variables;
 mod query_params;
 mod header_user_agent;
 mod header_custom_keyvalue;
-
+mod middleware_message;
 use hello_world::hello_world;
 use msg_body_json::msg_body_json;
 use text_body_string::text_body_string;
@@ -16,12 +16,22 @@ use path_variables::{hardcoded_path, path_variables};
 use query_params::query_params;
 use header_user_agent::header_user_agent;
 use header_custom_keyvalue::header_custom_keyvalue;
+use middleware_message::middleware_message;
 
+
+#[derive(Clone)]
+pub struct SharedData {
+    pub message: String,
+}
 
 pub fn create_routes() -> Router {
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
         .allow_origin(Any);
+
+    let shared_data = SharedData {
+        message: "Hello World from Shared Data".to_string(),
+    };
 
     Router::new().route("/", get(hello_world))
         .route("/text_body_string", post(text_body_string))  // curl -v POST http://localhost:3000/text_body_string -d "Mahalakshmi mata I need help"
@@ -31,5 +41,7 @@ pub fn create_routes() -> Router {
         .route("/query_params", post(query_params))  // curl -X POST "http://localhost:3000/query_params?message=Hello%20world&id=117"
         .route("/header_user_agent", get(header_user_agent))  // curl -H "User-Agent: samyakt" -H "Content-Type: application/json" http://localhost:3000/header_user_agent
         .route("/header_custom_keyvalue", get(header_custom_keyvalue))  // curl -H "x-message: Hello World from x-message custom header" http://localhost:3000/header_custom_keyvalue
+        .route("/middleware_message", get(middleware_message))  // curl http://localhost:3000/middleware_message
         .layer(cors)
+        .layer(Extension(shared_data))
 }
